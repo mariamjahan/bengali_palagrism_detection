a66010bb6cd221f93a7902bdad3986185ac64f55
6BIJOY6 <bijoyj66@gmail.com>
2025-07-11 23:47:30 +0600

    port change

--- main.py
+++ main.py
@@ -205,4 +205,4 @@ async def check_plagiarism(input: InputText):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8500)
